03247da41d05fa57e0a807368e132eb6821d3307
BrainLabsDev <admin@brainlabs.tech>
2024-07-02 20:33:14 +0200

    prueba piloto

---
+++ storage/framework/views/68512bd6db32e099fcf8511742e22da0.php
@@ -0,0 +1,2 @@
+<?php echo $__env->make("Layout::admin.app", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php /**PATH /Users/juanpi/Documents/laravel/gotrip/resources/views/admin/layouts/app.blade.php ENDPATH**/ ?>
